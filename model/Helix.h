df2ab4264f95dd0ae6390e2a761f280f06ded291
joaquind <oninthebest@yandex.ru>
2024-02-04 15:14:21 +0300

    Work: complete Helix class implementation

--- model/Helix.h
+++ model/Helix.h
@@ -5,10 +5,29 @@
 #ifndef INC_3DCURVES_HELIX_H
 #define INC_3DCURVES_HELIX_H
 
+#include <cmath>
+#include "Base.h"
+
 namespace crv {
 
-    class Helix {
+    class Helix : public BaseCurve {
+    public:
+        Helix() = default;
 
+        Helix(double radius, double step) : radius_{radius}, step_{step} {}
+
+        Point GetPoint(double angle) override {
+            double z_coordinate = angle / (2 * M_PI) * step_;
+            return {radius_ * cos(angle), radius_ * sin(angle), z_coordinate};
+        }
+
+        double GetFirstDerivative(double angle) override {
+            return tan(M_PI_2 + angle);
+        }
+
+    private:
+        double radius_{1};
+        double step_{1};
     };
 
 } // crv
