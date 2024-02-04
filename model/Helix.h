1d18c8cf0cc3863dc92a6447cbd99b05e46911e6
joaquind <oninthebest@yandex.ru>
2024-02-04 14:48:22 +0300

    Add: Helix class implementation

---
+++ model/Helix.h
@@ -0,0 +1,16 @@
+//
+// Created by Joaquina Daeron on 2/4/24.
+//
+
+#ifndef INC_3DCURVES_HELIX_H
+#define INC_3DCURVES_HELIX_H
+
+namespace crv {
+
+    class Helix {
+
+    };
+
+} // crv
+
+#endif //INC_3DCURVES_HELIX_H
